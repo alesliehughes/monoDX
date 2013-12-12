4bef2641523d97aa8f73172c311f2b4006d01145
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2013-12-12 11:18:16 +1100

    Initial Test projects

---
+++ Test.Microsoft.DirectX.Direct3D/Manager.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Test.Microsoft.DirectX.Direct3D
+{
+	[TestFixture()]
+	public class TestManager
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
---
+++ Test.Microsoft.DirectX/Vector2.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace Test.Microsoft.DirectX
+{
+	[TestFixture()]
+	public class TestVector2
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
