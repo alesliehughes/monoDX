1a274afcc069e556ce836272d2a64b88aad46718
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2017-05-05 12:33:45 +1000

    Add script to copy built binaries to single directory
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>

---
+++ copylibs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p output
+
+cp ./Microsoft.DirectX/bin/Debug/Microsoft.DirectX.dll output
+cp ./Microsoft.DirectX.Direct3DX/bin/Debug/Microsoft.DirectX.Direct3DX.dll output
+cp ./Microsoft.DirectX.DirectInput/bin/Debug/Microsoft.DirectX.DirectInput.dll output
+cp ./Microsoft.DirectX.DirectPlay/bin/Debug/Microsoft.DirectX.DirectPlay.dll output
+cp ./Microsoft.DirectX.Direct3D/bin/Debug/Microsoft.DirectX.Direct3D.dll output
+cp ./Microsoft.DirectX.DirectSound/bin/Debug/Microsoft.DirectX.DirectSound.dll output
